00cea5eb365d8c82f4d64149259edf8f8f98d724
Corto <cortopavie@gmail.com>
2020-03-06 11:42:11 +0100

    UI 1.0 + Gate

---
+++ SolarProto/Assets/1_Scripts/UI/PlanificationTime.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace SolarProto
+{
+    public class PlanificationTime : MonoBehaviour
+    {
+        [SerializeField] private float pti;
+        [SerializeField] private float ptu;
+        [SerializeField] private float timeCount;
+
+        [SerializeField] private Scrollbar slider;
+        [SerializeField] private float animDuration;
+
+        [SerializeField] private TextMeshProUGUI percentage;
+
+
+        [ContextMenu("Test")]
+        private void TestPTI()
+        {
+            SetTimeCount(45, 60);
+        }
+
+        public void SetTimeCount(float initValue, float subValue)
+        {
+            pti = initValue;
+            ptu = subValue;
+            timeCount = pti + ptu;
+            StartCoroutine(MoveScrollBar(timeCount, pti, ptu));
+        }
+
+        IEnumerator MoveScrollBar(float timeCount, float pti, float ptu)
+        {
+
+            float timer = 0f;
+
+            while (timer < animDuration)
+            {
+                slider.size = Mathf.Lerp(0, pti / timeCount, timer / animDuration);
+                percentage.text = (Mathf.Round(slider.size * 100)).ToString() + " %";
+
+                yield return null;
+                timer += Time.deltaTime;
+            }
+            slider.size = Mathf.Lerp(0, pti / timeCount, 1);
+
+
+        }
+    }
+}
+
---
+++ SolarProto/Assets/1_Scripts/UI/Score.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+namespace SolarProto
+{
+    public class Score : MonoBehaviour
+    {
+        [SerializeField] private int totalScore;
+
+        [SerializeField] TextMeshProUGUI scoreText;
+        [SerializeField] private float animDuration;
+
+        public void AddLevelInfos(int levelNumber, int numverOfMoves)
+        {
+            totalScore += levelNumber / numverOfMoves;
+        }
+
+        [ContextMenu("Test")]
+        public void LaunchCoroutine()
+        {
+            StartCoroutine(IncreaseScoreText(totalScore));
+        }
+
+        IEnumerator IncreaseScoreText(int totalScore)
+        {
+            float timer = 0f;
+
+            while (timer < animDuration)
+            {
+                scoreText.text = Mathf.Round(Mathf.Lerp(0, totalScore, timer / animDuration)).ToString() + " points";
+
+                yield return null;
+                timer += Time.deltaTime;
+            }
+
+            scoreText.text = Mathf.Round(Mathf.Lerp(0, totalScore, 1)).ToString() + " points";
+        }
+    }
+}
+
